f185667e31c2dc03ca323a92c3f8d123c4f76f69
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-10-24 14:46:28 +0200

    tests: added travis file (#2)
    * imported ds proxy
    
    * dapp install https://github.com/centrifuge/tinlake
    
    * added travis
    
    * same version for solidity

--- src/proxy.sol
+++ src/proxy.sol
@@ -1,8 +1,82 @@
-pragma solidity ^0.5.10;
+// proxy.sol - execute actions atomically through the proxy's identity
 
+// Copyright (C) 2017  DappHub, LLC
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity >=0.4.24;
+
+// Original DSProxy https://github.com/dapphub/ds-proxy
 contract Proxy {
 
+    constructor() public {
 
+    }
 
+    function() external payable {
+    }
+
+    function execute(address _target, bytes memory _data)
+    public
+    payable
+    returns (bytes memory response)
+    {
+        require(_target != address(0), "ds-proxy-target-address-required");
+
+        // call contract in current context
+        assembly {
+            let succeeded := delegatecall(sub(gas, 5000), _target, add(_data, 0x20), mload(_data), 0, 0)
+            let size := returndatasize
+
+            response := mload(0x40)
+            mstore(0x40, add(response, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+            mstore(response, size)
+            returndatacopy(add(response, 0x20), 0, size)
+
+            switch iszero(succeeded)
+            case 1 {
+            // throw if delegatecall failed
+                revert(add(response, 0x20), size)
+            }
+        }
+    }
 }
 
+// DSProxyFactory
+// This factory deploys new proxy instances through build()
+// Deployed proxy addresses are logged
+contract DSProxyFactory {
+    event Created(address indexed sender, address indexed owner, address proxy);
+    mapping(address=>bool) public isProxy;
+
+    constructor() public {
+    }
+
+    // deploys a new proxy instance
+    // sets owner of proxy to caller
+    function build() public returns (address payable proxy) {
+        proxy = build(msg.sender);
+    }
+
+    // deploys a new proxy instance
+    // sets custom owner of proxy
+    function build(address owner) public returns (address payable proxy) {
+        proxy = address(new Proxy());
+        emit Created(msg.sender, owner, address(proxy));
+
+        // todo create NFT with correct owner
+       // Proxy(proxy).setOwner(owner);
+        isProxy[proxy] = true;
+    }
+}
