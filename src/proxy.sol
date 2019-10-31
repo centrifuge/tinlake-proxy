bedf858cfdd57fa7c04cda1a0a87b89b81d8fa8d
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-10-31 11:49:27 +0100

    feat: proxy with NFT access token (#1)
    * imported ds proxy
    
    * dapp install https://github.com/centrifuge/tinlake
    
    * ds-proxy with NFT access token
    
    * added fail test cases
    
    * own test dir
    
    * added execute code

--- src/proxy.sol
+++ src/proxy.sol
@@ -1,27 +1,29 @@
-// proxy.sol - execute actions atomically through the proxy's identity
-
-// Copyright (C) 2017  DappHub, LLC
+// proxy.sol -- proxy contract for delegate calls
+// Copyright (C) 2019 Centrifuge
 
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
+// it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 pragma solidity >=0.4.24;
 
+import "tinlake/title.sol";
+
 // Original DSProxy https://github.com/dapphub/ds-proxy
-contract Proxy {
+contract Proxy is TitleOwned {
+    uint public accessToken;
 
-    constructor() public {
-
+    constructor(address title_, uint accessToken_) TitleOwned(title_) public {
+        accessToken = accessToken_;
     }
 
     function() external payable {
@@ -30,6 +32,7 @@ contract Proxy {
     function execute(address _target, bytes memory _data)
     public
     payable
+    owner(accessToken)
     returns (bytes memory response)
     {
         require(_target != address(0), "ds-proxy-target-address-required");
@@ -51,16 +54,43 @@ contract Proxy {
             }
         }
     }
+
+    function execute(bytes memory _code, bytes memory _data)
+    public
+    payable
+    owner(accessToken)
+    returns (address target, bytes memory response)
+    {
+        assembly {
+            target := create(0, add(_code, 0x20), mload(_code))
+            switch iszero(extcodesize(target))
+            case 1 {
+            // throw if contract failed to deploy
+                revert(0, 0)
+            }
+        }
+
+       response = execute(target, _data);
+    }
 }
 
-// DSProxyFactory
+contract TitleLike_ {
+    function count() public returns(uint);
+    function issue(address usr) public returns(uint);
+}
+
+
+// ProxyFactory
 // This factory deploys new proxy instances through build()
 // Deployed proxy addresses are logged
-contract DSProxyFactory {
+contract ProxyFactory {
     event Created(address indexed sender, address indexed owner, address proxy);
     mapping(address=>bool) public isProxy;
 
-    constructor() public {
+    TitleLike_ title;
+
+    constructor(address title_) public {
+        title = TitleLike_(title_);
     }
 
     // deploys a new proxy instance
@@ -72,11 +102,12 @@ contract DSProxyFactory {
     // deploys a new proxy instance
     // sets custom owner of proxy
     function build(address owner) public returns (address payable proxy) {
-        proxy = address(new Proxy());
-        emit Created(msg.sender, owner, address(proxy));
+        uint id = title.count();
+        proxy = address(new Proxy(address(title), id));
+        uint token = title.issue(owner);
+        require(id == token);
 
-        // todo create NFT with correct owner
-       // Proxy(proxy).setOwner(owner);
+        emit Created(msg.sender, owner, address(proxy));
         isProxy[proxy] = true;
     }
 }
