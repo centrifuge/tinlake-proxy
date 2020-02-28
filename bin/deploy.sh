20d1b19bb5530d1249b2f0e274aa651ebde841b8
Charly <charly@centrifuge.io>
2020-02-28 16:26:41 +0100

    fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,17 @@
-BIN_DIR=${BIN_DIR:-$(cd "${1%/*}"&&pwd)}
-
 # src env for contract deployment
-source $BIN_DIR/tinlake-proxy/bin/util.sh
-source $BIN_DIR/tinlake-proxy/bin/test/local_env.sh
+source ./util.sh
+source ./test/local_env.sh
+
 
 # create address file and build contracts
-touch $BIN_DIR/tinlake-proxy/test/addresses.json
+touch ./test/addresses.json
 
 dapp update && dapp build --extract
 
-export PROXY_REGISTRY=$(seth send --create $BIN_DIR/tinlake-proxy/out/ProxyRegistry.bin 'ProxyRegistry()')
+export PROXY_REGISTRY=$(seth send --create ./out/ProxyRegistry.bin 'ProxyRegistry()')
 message Proxy Registry Address: $PROXY_REGISTRY
 
-DEPLOYMENT_FILE=$BIN_DIR/tinlake-proxy/bin/test/addresses.json
+DEPLOYMENT_FILE=./test/addresses.json
 
 addValuesToFile $DEPLOYMENT_FILE <<EOF
 {
