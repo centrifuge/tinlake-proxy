ed2ebd90a94d7c23bce04a283fc517f349728671
Charly <charly@centrifuge.io>
2020-02-28 16:34:03 +0100

    fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,17 +1,19 @@
+BIN_DIR=${BIN_DIR:-$(cd "${0%/*}"&&pwd)}
+
 # src env for contract deployment
-source ./bin/util.sh
-source ./bin/test/local_env.sh
+source $BIN_DIR/util.sh
+source $BIN_DIR/test/local_env.sh
 
 
 # create address file and build contracts
-touch ./bin/test/addresses.json
+touch $BIN_DIR/test/addresses.json
 
 dapp update && dapp build --extract
 
 export PROXY_REGISTRY=$(seth send --create ./out/ProxyRegistry.bin 'ProxyRegistry()')
 message Proxy Registry Address: $PROXY_REGISTRY
 
-DEPLOYMENT_FILE=./bin/test/addresses.json
+DEPLOYMENT_FILE=$BIN_DIR/test/addresses.json
 
 addValuesToFile $DEPLOYMENT_FILE <<EOF
 {
