c810825e901eb8893345fb6fa31c355c9223d3a4
Charly <fei.charleen@gmail.com>
2020-02-28 17:15:39 +0100

    fix: path (#12)
    * deployments

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,17 @@
-BIN_DIR=${BIN_DIR:-$(cd "${1%/*}"&&pwd)}
+BIN_DIR=${BIN_DIR:-$(cd "${0%/*}"&&pwd)}
 
 # src env for contract deployment
-source $BIN_DIR/bin/util.sh
-source $BIN_DIR/bin/test/local_env.sh
+source $BIN_DIR/util.sh
+source $BIN_DIR/test/local_env.sh
 
 # create address file and build contracts
-touch $BIN_DIR/bin/test/addresses.json
+DEPLOYMENT_FILE=$BIN_DIR/../deployments/addresses_$(seth chain).json
+touch $DEPLOYMENT_FILE
 
 dapp update && dapp build --extract
 
 export PROXY_REGISTRY=$(seth send --create ./out/ProxyRegistry.bin 'ProxyRegistry()')
 message Proxy Registry Address: $PROXY_REGISTRY
-
-DEPLOYMENT_FILE=$BIN_DIR/bin/test/addresses.json
 
 addValuesToFile $DEPLOYMENT_FILE <<EOF
 {
