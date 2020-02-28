e19f06a736304096c7f6fa28d5b1c1f591566ed7
Charly <fei.charleen@gmail.com>
2020-02-28 17:44:33 +0100

    Deploy scripts (#13)
    * space

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,13 +5,15 @@ source $BIN_DIR/util.sh
 source $BIN_DIR/test/local_env.sh
 
 # create address file and build contracts
-DEPLOYMENT_FILE=$BIN_DIR/../deployments/addresses_$(seth chain).json
-touch $DEPLOYMENT_FILE
-
 dapp update && dapp build --extract
 
 export PROXY_REGISTRY=$(seth send --create ./out/ProxyRegistry.bin 'ProxyRegistry()')
 message Proxy Registry Address: $PROXY_REGISTRY
+
+cd $BIN_DIR
+DEPLOYMENT_FILE=../deployments/addresses_$(seth chain).json
+
+touch $DEPLOYMENT_FILE
 
 addValuesToFile $DEPLOYMENT_FILE <<EOF
 {
