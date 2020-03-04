e6465090cd4fa011815802d2205429da2dbb59ed
ilinzweilin <alina@centrifuge.io>
2020-03-04 18:14:24 +0100

    fix: deploy-script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,20 +1,21 @@
 PROXY_BIN_DIR=${PROXY_BIN_DIR:-$(cd "${0%/*}"&&pwd)}
-
+cd $PROXY_BIN_DIR
 # src env for contract deployment
 source $PROXY_BIN_DIR/util.sh
 source $PROXY_BIN_DIR/test/local_env.sh
 
 # create address file and build contracts
+
+cd $PROXY_BIN_DIR/../
+
 dapp update && dapp build --extract
 
+cd $PROXY_BIN_DIR
 # create deployment folder
 mkdir $PROXY_BIN_DIR/../deployments
 
-
-export PROXY_REGISTRY=$(seth send --create ./out/ProxyRegistry.bin 'ProxyRegistry()')
+export PROXY_REGISTRY=$(seth send --create $PROXY_BIN_DIR/../out/ProxyRegistry.bin 'ProxyRegistry()')
 message Proxy Registry Address: $PROXY_REGISTRY
-
-cd $PROXY_BIN_DIR
 
 DEPLOYMENT_FILE=$PROXY_BIN_DIR/../deployments/addresses_$(seth chain).json
 
